2297bff3637afa062665ad407690d74f763e42eb
Adem <ademcran4aliev@abv.bg>
2025-04-21 11:23:07 +0300

    fixes

--- invitation/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { InvitationComponent } from './invitation/invitation.component';
-
-export const routes: Routes = [
-    { path: '', component: InvitationComponent }
-];
